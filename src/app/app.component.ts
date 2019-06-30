c65cd5bb1efbd5150a74e2f2a9c90c7ba460768a
ep <eiphyuphyuthant97@gmail.com>
2019-06-30 21:24:14 +0630

    This is angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactiveForm-app';
+
+  constructor(private  formBuilder:FormBuilder){
+
+  }
+  /*userForm =new FormGroup(
+    {
+      name: new FormControl(''),
+      password:new FormControl(''),
+      email: new FormControl('')
+    }
+  )*/
+
+  userForm = this.formBuilder.group({
+    name:['',[Validators.required,Validators.minLength(3)]],
+    password:[''],
+    email:[''],
+    alternateEmails: this.formBuilder.array([])
+  })
+
+  get alternateEmails(){
+    return this.userForm.get('alternateEmails') as FormArray
+  }
+
+  addAlternateEmails(){
+    this.alternateEmails.push(this.formBuilder.control(''))
+  }
+
 }
